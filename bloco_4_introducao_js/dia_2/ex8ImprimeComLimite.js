98eed0e6cd71c94b5b4bea0624eb14b5c676a468
renatomak <renato.mark.silva@gmail.com>
2020-08-29 11:37:49 -0300

    Criar vetor dentro do limite pedido

---
+++ bloco_4_introducao_js/dia_2/ex8ImprimeComLimite.js
@@ -0,0 +1,8 @@
+/*8 - Utilizando for, crie uma array que vá de 1 até 25 e imprima o resultado;*/
+let numbers = [];
+
+for( let i = 1; i <= 25; i +=1 ){
+    numbers.push(i);
+}
+
+console.log(numbers);
