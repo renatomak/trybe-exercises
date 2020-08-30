b701f7cfc830c9540042dc56665ba97cc14b9df2
renatomak <renato.mark.silva@gmail.com>
2020-08-29 21:46:56 -0300

    Imprime quadrado com asterisco

--- bloco_4_introducao_js/dia_3/exercicio1Logica.js
+++ bloco_4_introducao_js/dia_3/exercicio1Logica.js
@@ -1,4 +1,5 @@
 /**1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. */
+
 const valor = 5;
 let contador = valor;
 
