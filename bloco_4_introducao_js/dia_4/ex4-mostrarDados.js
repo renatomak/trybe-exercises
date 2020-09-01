86adb0cde42b32eca42239f94eecf9bfa72c5f30
renatomak <renato.mark.silva@gmail.com>
2020-09-01 15:26:49 -0300

    Ex. 4: Mesclar objetos

---
+++ bloco_4_introducao_js/dia_4/ex4-mostrarDados.js
@@ -0,0 +1,32 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+  }; 
+
+  let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim",
+  }; 
+
+  let resultado = "";
+
+
+
+  function imprimeChaveObjeto(obj1, obj2){
+    let resultado = [];
+
+        for(const i in obj1){
+            if(i === "recorrente"){
+                resultado[i] = "Ambos recorrentes"
+            }else{
+                resultado[i] = obj1[i] + " " + obj2[i]
+            }
+            console.log(resultado[i]);
+        }
+  }
+
+  imprimeChaveObjeto(info, info2);
