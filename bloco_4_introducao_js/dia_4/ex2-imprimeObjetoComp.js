856834fff53078aa3303b829a1ce313805a130b5
renatomak <renato.mark.silva@gmail.com>
2020-09-01 14:00:01 -0300

    Ex. 3: imprimir objeto completo

---
+++ bloco_4_introducao_js/dia_4/ex2-imprimeObjetoComp.js
@@ -0,0 +1,12 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "sim",
+  }; 
+
+  function imprimeObjeto(obj){
+          console.log(obj);
+  }
+
+  imprimeObjeto(info);
