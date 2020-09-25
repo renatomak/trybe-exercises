7e4dee3b34e013c7e8f6b02d81ed359e4a389fd5
renatomak <renato.mark.silva@gmail.com>
2020-09-25 19:10:53 -0300

    exercicio 4

--- bloco_7/dia_1/script.js
+++ bloco_7/dia_1/script.js
@@ -38,6 +38,20 @@ const longestWord = (frase) => {
 }
 
 
+function funcao1(string){
+    return `Ele quebrou a xícara.
+    Eu uso o chapéu.
+    A fumaça sai pela chaminé.
+    O bebê está chorando.
+    Papai está cochilando.
+    O jogador chuta a bola.
+    Ela toma xarope.
+    Xingar não é legal.
+    O xerife toma conta.
+    A Maria está usando um xale.`.replace(/x/gi, string);
+}
+
 console.log(fatorial(5))
 console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
+console.log(funcao1('FF'))
 
