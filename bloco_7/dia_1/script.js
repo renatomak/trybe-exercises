47a03bd385e8a1a0ab92afbf34b8283c9a46d88f
renatomak <renato.mark.silva@gmail.com>
2020-09-28 16:05:41 -0300

    inclusão do arquivo .js

--- bloco_7/dia_1/script.js
+++ bloco_7/dia_1/script.js
@@ -38,8 +38,8 @@ const longestWord = (frase) => {
 }
 
 
-function funcao1(string){
-    return `Ele quebrou a xícara.
+/* function funcao1(string){
+    /* `Ele quebrou a xícara.
     Eu uso o chapéu.
     A fumaça sai pela chaminé.
     O bebê está chorando.
@@ -48,10 +48,18 @@ function funcao1(string){
     Ela toma xarope.
     Xingar não é legal.
     O xerife toma conta.
-    A Maria está usando um xale.`.replace(/x/gi, string);
+    A Maria está usando um xale.`
+    return  `Trybe x aqui`.replace(/x/gi, string);
+}
+ */
+
+let array = 'JavaScript, HTML, CSS, REACT, nodejs'
+
+funcao1 = string => {
+    return array.replace(/a/gi, string);
 }
 
 console.log(fatorial(5))
 console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"));
-console.log(funcao1('FF'))
+console.log(funcao1('Bebeto'))
 
