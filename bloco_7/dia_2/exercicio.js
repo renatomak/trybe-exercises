650c5d868dda322de1da2f3e1de9f818c918f262
renatomak <renato.mark.silva@gmail.com>
2020-09-28 19:10:23 -0300

    Exercicios 5 e 6

--- bloco_7/dia_2/exercicio.js
+++ bloco_7/dia_2/exercicio.js
@@ -36,8 +36,28 @@ function listValuesObject(obj) {
   console.log(Object.values(obj))
 }
 
+let allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+
+function totalAlunos(objs){
+  let total = 0;
+  /* const chaves = Object.keys(objs);
+  for(let i = 0; i < Object.keys(objs).length; i += 1){
+    total += objs[chaves[i]].numeroEstudantes;
+  } */
+
+  for(const key in objs){
+    total += objs[key].numeroEstudantes;
+  }
+
+  return total;
+}
+
 //console.log(lesson2);
 //console.log(lesson3.length)
 //showKeys(lesson3);
 //console.log('Tamanho = ' + sizeObject(lesson1));
-listValuesObject(lesson1);
+//listValuesObject(lesson1);
+//console.log(allLessons)
+
+console.log(totalAlunos(allLessons));
+
