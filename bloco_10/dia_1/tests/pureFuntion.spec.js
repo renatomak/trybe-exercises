a897fc49e2e47091c366117957c8dc558e24464c
renatomak <renato.mark.silva@gmail.com>
2020-11-01 21:37:12 -0300

    exercicio bonus 1

---
+++ bloco_10/dia_1/tests/pureFuntion.spec.js
@@ -0,0 +1,47 @@
+// setPrice(item: Object, price: Number) => item: Object
+const setPrice = (item, price) => {
+  item.price = price;
+  return item;
+};
+
+// addToCart(cart: Array, item: Object) => cart: Array
+const addToCart = (cart, item) => {
+  cart.name = item;
+  return cart;
+};
+
+describe('setPrice()', () => {
+  it('should set the price in the given item object, immutably.', () => {
+    const item = {
+      name: 'test',
+      price: 30,
+    };
+    const copy = Object.assign({}, item);
+
+    const actual = setPrice(item, 50);
+    const expected = {
+      name: 'test',
+      price: 50,
+    };
+
+    expect(actual).toEqual(expected);
+    expect(item).toEqual(copy);
+  });
+});
+
+describe('addToCart()', () => {
+  it('should add item to cart, immutably', () => {
+    const originalCart = [];
+    const item = {
+      name: 'Violator',
+      price: 30,
+    };
+    const copy = originalCart.slice();
+
+    const actual = addToCart(originalCart, item);
+    const expected = [item];
+
+    expect(actual).toEqual(expected);
+    expect(originalCart).toEqual(copy);
+  });
+});
