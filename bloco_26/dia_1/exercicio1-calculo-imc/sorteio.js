0f0204bd0751fe64545ae17e750e561a5c14692e
renatomak <renato.mark.silva@gmail.com>
2021-03-26 09:56:30 -0300

    Exercicio 7

---
+++ bloco_26/dia_1/exercicio1-calculo-imc/sorteio.js
@@ -0,0 +1,22 @@
+const readLine = require('readline-sync');
+let laco = 1
+
+function gerarNumeroAleatorio() {
+  const numRandon = Math.round(Math.random() * 10 );
+
+  return numRandon;
+}
+
+while(laco) {
+  const aleatorio = gerarNumeroAleatorio();
+
+  const escolha = readLine.questionInt("Informe o número escolhido: ")
+  console.log("Numero aleatorio = ", aleatorio, "\nNúmero escolhido = ", escolha)
+  if(escolha === aleatorio) {
+    console.log("\n Parabéns, número correto!")
+  } else {
+    console.log("Opa, não foi dessa vez. O número era ", aleatorio)
+  }
+
+  laco = readLine.questionInt("Deseja continuar? Digit 1 - sim ou 0 - não: ");
+}
