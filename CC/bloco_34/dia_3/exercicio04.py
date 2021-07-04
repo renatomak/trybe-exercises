23aca9999797cbd48e40fdd64f0952f26062acaa
renatomak <renato.mark.silva@gmail.com>
2021-07-04 11:35:20 -0300

    exercicio 4 e 5

--- CC/bloco_34/dia_3/exercicio04.py
+++ CC/bloco_34/dia_3/exercicio04.py
@@ -1,11 +1,14 @@
 import requests
 from parsel import Selector, selector
-
-res = requests.get('http://books.toscrape.com/catalogue/the-grand-design_405/index.html')
+URL = 'http://books.toscrape.com/catalogue/the-grand-design_405/index.html'
+res = requests.get(URL)
 selector = Selector(text=res.text)
 
 title = selector.css('.product_main h1::text').get()
 price = selector.css('.price_color::text').re_first(r"\d+\.\d{2}")
-desciption = selector.css("#product_description ~ p::text").get()[:-len("...more")]
+desciption = selector.css("#product_description ~ p::text").get()[:-len(" ...more")]
+url_img = selector.css('.active img::attr(src)').get()
+url_img = URL + url_img
+amount = selector.css('.instock::text').re_first('\d')
 
-print(title+',', price+',',  desciption)
+print(title, price,  desciption, url_img, amount, sep=', ')
