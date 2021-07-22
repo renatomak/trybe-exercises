04e8fc1daf829f02ee3e5a73d2cc12331e204ef9
renatomak <renato.mark.silva@gmail.com>
2021-07-21 21:06:06 -0300

    Exercicio 1

---
+++ CC/bloco_35/dia_2/tv.py
@@ -0,0 +1,23 @@
+class TV:
+    def __init__(self, tamanho, ligada):
+        self.volume = 50
+        self.canal = 1
+        self.tamanho = tamanho
+        self.ligada = ligada
+
+    def aumentar_volume(self):
+        if 0 >= self.volume <= 99:
+            self.volume += 1
+
+    def diminuir_volume(self):
+        if 0 >= self.volume <= 99:
+            self.volume -= 1
+
+    def modificar_canal(self, canal):
+        try:
+            self.canal = canal
+        except ValueError:
+            print('Error: canal invalido')
+
+    def ligar_desligar(self):
+        self.ligada = True if self.ligada else False
