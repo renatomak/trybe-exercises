3d2e56818b227d7ffd5968113dccafcc198c442a
renatomak <renato.mark.silva@gmail.com>
2021-08-14 09:55:53 -0300

    exercicio 03

--- CC/bloco_37/exercicio_03.py
+++ CC/bloco_37/exercicio_03.py
@@ -1,11 +1,11 @@
 # Exemplo 1:
-# produtos = [1, 3, 1, 1, 2, 3]
+produtos = [1, 3, 1, 1, 2, 3]
 # resultado = 4
 # Os índices (0, 2), (0, 3), (1, 5), (2, 3) formam combinações.
 
 
 # Exemplo 2:
-produtos = [1, 1, 2, 3]
+# produtos = [1, 1, 2, 3]
 # resultado = 1
 # Os índices (0, 1) formam a única combinação.
 
@@ -18,7 +18,9 @@ def good_combinations(array):
     #         if array[x] == array[y] and y != x and y > x:
     #             result.append((x, y))
     # return result
-    return [(x, y) for x in range(size) for y in range(size) if array[x] == array[y] and y > x]
+    result = [(x, y) for x in range(size)
+              for y in range(size) if array[x] == array[y] and y > x]
+    return len(result)
 
 
 print(good_combinations(produtos))
